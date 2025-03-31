6347cb398144a554f20dac7bd5244111eadc6236
pc-new <pengchengneo@gmail.com>
2025-03-31 15:26:21 +0800

    fix

--- model/group.go
+++ model/group.go
@@ -1,9 +1,7 @@
 package model
 
 type Group struct {
-	Id        int    `json:"id" gorm:"primaryKey"`
-	Name      string `json:"name" gorm:"type:varchar(255);uniqueIndex;not null;default:''"`
-	Ratio     int    `json:"ratio" gorm:"type:int;not null;default:0"`
-	CreatedAt int64  `json:"created_at" gorm:"autoCreateTime"`
-	UpdatedAt int64  `json:"updated_at" gorm:"autoUpdateTime"`
+	Id    int    `json:"id" gorm:"primaryKey"`
+	Name  string `json:"name" gorm:"type:varchar(255);uniqueIndex;not null;default:''"`
+	Ratio int    `json:"ratio" gorm:"type:int;not null;default:0"`
 }
