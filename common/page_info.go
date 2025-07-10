ff4fc57f8e9f45ddbb6da8a913caf8978e3c6354
feitianbubu <feitianbubu@qq.com>
2025-07-10 15:45:58 +0000

    feat: get right page size

--- common/page_info.go
+++ common/page_info.go
@@ -2,14 +2,13 @@ package common
 
 import (
 	"github.com/gin-gonic/gin"
-	"strconv"
 )
 
 type PageInfo struct {
-	Page           int   `json:"page"`            // page num 页码
-	PageSize       int   `json:"page_size"`       // page size 页大小
-	StartTimestamp int64 `json:"start_timestamp"` // 秒级
-	EndTimestamp   int64 `json:"end_timestamp"`   // 秒级
+	Page           int   `json:"page" form:"p"`              // page num 页码
+	PageSize       int   `json:"page_size" form:"page_size"` // page size 页大小
+	StartTimestamp int64 `json:"start_timestamp"`            // 秒级
+	EndTimestamp   int64 `json:"end_timestamp"`              // 秒级
 
 	Total int `json:"total"` // 总条数，后设置
 	Items any `json:"items"` // 数据，后设置
@@ -46,13 +45,7 @@ func GetPageQuery(c *gin.Context) (*PageInfo, error) {
 		return nil, err
 	}
 	if pageInfo.Page < 1 {
-		// 兼容
-		page, _ := strconv.Atoi(c.Query("p"))
-		if page != 0 {
-			pageInfo.Page = page
-		} else {
-			pageInfo.Page = 1
-		}
+		pageInfo.Page = 1
 	}
 
 	if pageInfo.PageSize == 0 {
