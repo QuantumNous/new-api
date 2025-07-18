72eb1478b091a828bf99b02855ac13e4f6e07c47
linzero <root@linli.eu.org>
2025-07-19 00:05:54 +0800

    feat: add edgeone configuration and update API request forwarding functions

--- web/vite.config.js
+++ web/vite.config.js
@@ -37,6 +37,7 @@ export default defineConfig({
   },
   build: {
     rollupOptions: {
+      external: ['antd'],
       output: {
         manualChunks: {
           'react-core': ['react', 'react-dom', 'react-router-dom'],
