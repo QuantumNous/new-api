158ae5facbe0ece8299892d71ff89ef28c3ab607
comeback01 <219462554+ScioNos@users.noreply.github.com>
2026-01-09 14:49:26 +0100

    fix(web): wrap language selector trigger in div to fix React 19 ref issue

--- web/src/components/layout/headerbar/LanguageSelector.jsx
+++ web/src/components/layout/headerbar/LanguageSelector.jsx
@@ -75,13 +75,15 @@ const LanguageSelector = ({ currentLang, onLanguageChange, t }) => {
         </Dropdown.Menu>
       }
     >
-      <Button
-        icon={<Languages size={18} />}
-        aria-label={t('common.changeLanguage')}
-        theme='borderless'
-        type='tertiary'
-        className='p-1.5! text-current! focus:bg-semi-color-fill-1! dark:focus:bg-gray-700! rounded-full! bg-semi-color-fill-0! dark:bg-semi-color-fill-1! hover:bg-semi-color-fill-1! dark:hover:bg-semi-color-fill-2!'
-      />
+      <div style={{ display: 'inline-block' }}>
+        <Button
+          icon={<Languages size={18} />}
+          aria-label={t('common.changeLanguage')}
+          theme='borderless'
+          type='tertiary'
+          className='p-1.5! text-current! focus:bg-semi-color-fill-1! dark:focus:bg-gray-700! rounded-full! bg-semi-color-fill-0! dark:bg-semi-color-fill-1! hover:bg-semi-color-fill-1! dark:hover:bg-semi-color-fill-2!'
+        />
+      </div>
     </Dropdown>
   );
 };
