d5e73893435e5fc884ffa2cf98f861a6926caee8
Mison <liangmingsen@qdama.cn>
2026-03-26 17:36:14 +0800

    test: fix channel cache scan error fixture

--- model/channel_cache_test.go
+++ model/channel_cache_test.go
@@ -167,7 +167,7 @@ func TestInitChannelCacheKeepsPreviousSnapshotOnScanError(t *testing.T) {
 		"broken-channel",
 		"broken-model",
 		"default",
-		`{"is_multi_key":false,"multi_key_size":0,"multi_key_status_list":{},"multi_key_disabled_reason":{},"multi_key_disabled_time":{},"multi_key_polling_index":0,"multi_key_mode":0}`,
+		`{invalid`,
 		"",
 	).Error)
 
