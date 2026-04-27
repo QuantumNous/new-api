4bb62de68b89472cb98c952cbab1615d46eb4890
zl <zhangliangiloveyou@outlook.com>
2026-04-27 16:02:30 +0800

    release: 版本升至v0.13.1，打包优化

--- scripts/build-and-docker.ps1
+++ scripts/build-and-docker.ps1
@@ -7,7 +7,7 @@
 param(
     [string]$dockerHubUsername = "beyondandforever",
     [string]$imageName = "gq-api",
-    [string]$version = "",
+    [string]$version = "v0.13.1",
     [switch]$skipPush = $false
 )
 
