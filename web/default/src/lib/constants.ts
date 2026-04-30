2c468a6b20fe45b08d741ccf0057044e07457f36
Micah-Zheng <102610064+Micah-Zheng@users.noreply.github.com>
2026-04-30 11:56:21 +0800

    use cache-busted custom logo assets

--- web/default/src/lib/constants.ts
+++ web/default/src/lib/constants.ts
@@ -4,7 +4,7 @@
 
 // System Configuration Defaults
 export const DEFAULT_SYSTEM_NAME = 'New API'
-export const DEFAULT_LOGO = '/logo.png'
+export const DEFAULT_LOGO = '/logo-custom.png'
 
 // LocalStorage Keys
 export const STORAGE_KEYS = {
