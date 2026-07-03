5c568a1c28a4a5a5a3fe91ea13e428a0ed0d22e6
alaningtrump <alaningtrump@outlook.com>
2026-07-03 10:52:35 +0800

    refactor: use the built-in max/min to simplify the code
    Signed-off-by: alaningtrump <alaningtrump@outlook.com>

--- controller/channel.go
+++ controller/channel.go
@@ -360,14 +360,8 @@ func SearchChannels(c *gin.Context) {
 	}
 
 	total := len(channelData)
-	startIdx := (page - 1) * pageSize
-	if startIdx > total {
-		startIdx = total
-	}
-	endIdx := startIdx + pageSize
-	if endIdx > total {
-		endIdx = total
-	}
+	startIdx := min((page-1)*pageSize, total)
+	endIdx := min(startIdx+pageSize, total)
 
 	pagedData := channelData[startIdx:endIdx]
 
@@ -1565,10 +1559,7 @@ func ManageMultiKeys(c *gin.Context) {
 
 		// Calculate range for current page
 		start := (page - 1) * pageSize
-		end := start + pageSize
-		if end > filteredTotal {
-			end = filteredTotal
-		}
+		end := min(start+pageSize, filteredTotal)
 
 		// Get the page data
 		var pageKeyStatusList []KeyStatus
