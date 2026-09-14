8d522e30ba412bcac0943be559c261dba28a7f20
seefs001 <i@seefs.me>
2026-09-14 12:28:39 +0800

    fix: sglang icon

--- web/src/lib/lobe-icon.tsx
+++ web/src/lib/lobe-icon.tsx
@@ -28,9 +28,20 @@ For commercial licensing, please contact support@quantumnous.com
 import * as LobeIcons from '@lobehub/icons'
 import type React from 'react'
 
+import sglangLogo from '@/assets/brand-icons/sglang.svg'
 import { IconSub2api } from '@/assets/custom/icon-sub2api'
 
 const CUSTOM_ICONS: Record<string, React.ComponentType<{ size?: number }>> = {
+  SGLang: (props) => (
+    <img
+      src={sglangLogo}
+      alt=''
+      aria-hidden='true'
+      width={props.size ?? 20}
+      height={props.size ?? 20}
+      className='object-contain'
+    />
+  ),
   Sub2API: IconSub2api,
 }
 
